6585d4fe121f1c5a5258ce3bec812443dc5bdbbd
yuchangseuk <ckdtjr428@naver.com>
2024-04-12 16:32:44 +0900

    new page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Home from './pages/Home';
-import './styles.css';
+// import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./styles.css";
 function App() {
-
   return (
     <div>
-      <Home />
+      <Login />
+      {/* <Home /> */}
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import styles from "./Login.module.css";
+
+function Login() {
+  return <>안녕</>;
+}
+
+export default Login;
