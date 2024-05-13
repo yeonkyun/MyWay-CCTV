0a33df1a86fc1a7066c735ce8e682bcc8f03fbad
Jeong Yeon Kyun <jungyk411@gmail.com>
2024-05-13 23:45:39 +0900

    Modify Login.js, index.js, App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <div><Login /></div>
   );
 }
 
