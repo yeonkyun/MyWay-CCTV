8dad313daf0fb5903af5deaedc997ef80f2ceeb9
yuchangseuk <ckdtjr428@naver.com>
2024-06-01 12:13:55 +0900

    Layout change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Styles/Home.module.css";
 import Cctv from "../components/Cctv.js";
-import Smallcctv from "../components/Smallcctv.js";
+import Smallcctv from "../components/Smallcctv.js"; // 사용되지 않음
 import Kakao from "../components/Kakao.js";
 
 function Home() {
+  const [showCctv, setShowCctv] = useState(false); // 초기값을 false로 설정
+  const [cctvCount, setCctvCount] = useState(0); // CCTV 개수를 저장하는 상태 변수
+
+  const toggleCctvVisibility = () => {
+    setShowCctv(!showCctv);
+  };
+
+  const addCctv = () => {
+    setCctvCount(cctvCount + 1);
+  };
+
+  console.log("Show CCTV:", showCctv);
+  console.log("CCTV Count:", cctvCount);
+
   return (
     <div className={styles.container}>
-      <div className={styles.find_way}></div>
       <div className={styles.map_wrap}>
         <Kakao />
       </div>
-      {/* <div className={styles.cctvzone}>
-        <div className={styles.cctv_container}></div>
-        <div className={styles.cctv_list}>
-          <Cctv />
-          <Cctv />
-          <Cctv />
+      <button onClick={toggleCctvVisibility} className={styles.toggleButton}>
+        {showCctv ? "Hide CCTV" : "Show CCTV"}
+      </button>
+      {showCctv && (
+        <div className={styles.cctvContainer}>
+          {cctvCount > 0 && (
+            <div
+              className={cctvCount === 1 ? styles.largeCctv : styles.cctvGrid}
+            >
+              {[...Array(cctvCount)].map((_, index) => (
+                <Cctv key={index} />
+              ))}
+            </div>
+          )}
+          <button onClick={addCctv} className={styles.addButton}>
+            Add CCTV
+          </button>
         </div>
-        <div className={styles.smallContainer}>
-          <Smallcctv />
-          <Smallcctv />
-          <Smallcctv />
-          <Smallcctv />
-          <Smallcctv />
-          <Smallcctv />
-          <Smallcctv />
-        </div>
-      </div> */}
+      )}
     </div>
   );
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import styles from "../Styles/Home.module.css"; // 필요한 스타일 가져오기
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import styles from "../Styles/Layout.module.css"; // layout 스타일 가져오기
 import Btn_menu from "./Btn_menu"; // Btn_menu 경로가 올바른지 확인
-import { Link } from "react-router-dom";
 
 const Layout = () => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [activeButton, setActiveButton] = useState(null);
+  const [startLocation, setStartLocation] = useState("");
+  const [endLocation, setEndLocation] = useState("");
+
+  const toggleSearch = () => {
+    setShowSearch(!showSearch);
+  };
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  const handleSearch = () => {
+    console.log("Start:", startLocation);
+    console.log("End:", endLocation);
+    // 검색 로직 추가
+  };
+
   return (
     <div className={styles.mainContainer}>
       <header className={styles.menubar}>
@@ -18,25 +36,44 @@ const Layout = () => {
           </a>
         </h1>
         <ul className={styles.list_navbar}>
-          <li>
+          <li
+            className={activeButton === "search" ? styles.btn_menu_active : ""}
+          >
             <Btn_menu
               img_src={"image/baseline_search_black_24dp.jpg"}
               alt_text="search"
+              onClick={() => {
+                toggleSearch();
+                handleButtonClick("search");
+              }}
             />
           </li>
-          <li>
+          <li className={activeButton === "star" ? styles.btn_menu_active : ""}>
             <Btn_menu
               img_src={"image/baseline_star_black_24dp.jpg"}
               alt_text="star"
+              onClick={() => handleButtonClick("star")}
             />
           </li>
-          <li className={styles.li_login}>
-            <Link to="/login" className={styles.login}>
+          <li
+            className={`${styles.li_login} ${
+              activeButton === "login" ? styles.btn_menu_active : ""
+            }`}
+          >
+            <Link
+              to="/login"
+              className={styles.login}
+              onClick={() => handleButtonClick("login")}
+            >
               <Btn_menu img_src={"image/user.png"} alt_text="로그인" />
             </Link>
           </li>
-          <li>
-            <Link to="/공지사항" className={styles.add_icon}>
+          <li className={activeButton === "more" ? styles.btn_menu_active : ""}>
+            <Link
+              to="/공지사항"
+              className={styles.add_icon}
+              onClick={() => handleButtonClick("more")}
+            >
               <Btn_menu
                 img_src={"image/baseline_more_horiz_black_24dp.jpg"}
                 alt_text="더보기"
@@ -45,6 +82,28 @@ const Layout = () => {
           </li>
         </ul>
       </header>
+      {showSearch && (
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            placeholder="Start location"
+            value={startLocation}
+            onChange={(e) => setStartLocation(e.target.value)}
+            className={styles.searchInput}
+          />
+          <input
+            type="text"
+            placeholder="End location"
+            value={endLocation}
+            onChange={(e) => setEndLocation(e.target.value)}
+            className={styles.searchInput}
+          />
+          <button onClick={handleSearch} className={styles.searchButton}>
+            Search
+          </button>
+          <div onClick={toggleSearch} className={styles.closeButton}></div>
+        </div>
+      )}
       <Outlet /> {/* This is where the nested routes will render */}
     </div>
   );
